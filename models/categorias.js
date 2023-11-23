e60caac460bece9d7dfea8c1bc94665826306437
daylucero <puemaam@gmail.com>
2023-11-23 00:00:26 -0300

    agregué categorías; y libros para poder redigir desde el index al libro en particular

--- models/categorias.js
+++ models/categorias.js
@@ -16,8 +16,10 @@ const cat8 = new CategoriaLibro(8, 'Filosofía');
 const cat9 = new CategoriaLibro(9, 'Cocina');
 const cat10 = new CategoriaLibro(10, 'Drama');
 const cat11 = new CategoriaLibro(11, 'Novela');
-const cat12 = new CategoriaLibro(11, 'Aventura');
+const cat12 = new CategoriaLibro(12, 'Aventura');
+const cat13 = new CategoriaLibro(13, 'Política');
+const cat14 = new CategoriaLibro(14, 'Desarrollo Personal');
 
-const categorias = [cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12];
+const categorias = [cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12, cat13, cat14];
 
 module.exports = categorias;
