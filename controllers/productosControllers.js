f32576a4548cae605540147248d4a283aed6d1ab
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2023-12-04 21:10:17 -0300

    corrigiendo error de tipeo

--- controllers/productosControllers.js
+++ controllers/productosControllers.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const libros = JSON.parse( fs.readFileSync(path.join(process.cwd(), './data/libros.json')),'utf-8');
-const usuarios = JSON.parse(fs.readFileSync(path.join(process.cwd(), './data/users.json')), 'utf-9');
+const usuarios = JSON.parse(fs.readFileSync(path.join(process.cwd(), './data/users.json')), 'utf-8');
 const categorias = JSON.parse(fs.readFileSync(path.join(process.cwd(), './data/categorias.json')), 'utf-8')
 
 
