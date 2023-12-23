8031fcc5ac5c937002531d0cc0498be27f5840c6
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2023-12-23 18:31:28 -0300

    añado input para imagenes en formulario de registro

--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,24 @@ const express = require ('express');
 const mainController = require ('../controllers/mainControllers.js')
 const router = express.Router();
 
+const multer = require('multer');
+const path = require('path');
+
+// ************ Multer Storage ************
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(process.cwd(),'public/images/profileImages'));
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_user_pfile_img${path.extname(file.originalname)}`);
+    }
+});
+  
+const uploadFile = multer({storage});
+
 router.get('/', mainController.index);
 router.get('/login', mainController.login);
-router.get('/register', mainController.register)
+
+router.get('/register', mainController.register);
 
 module.exports = router;
