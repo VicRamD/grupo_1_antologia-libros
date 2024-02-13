9865d53d812a0edd8d002010429ebe127cceec0a
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2024-02-12 21:46:18 -0300

    Modifico validación de libros

--- middlewares/bookValidation.js
+++ middlewares/bookValidation.js
@@ -3,6 +3,8 @@ const path = require('path');
 const fs = require('fs');
 const finders = require('../utils/finders');
 
+const db = require('../database/models');
+
 const users = JSON.parse(fs.readFileSync(path.join(process.cwd(), '/data/users.json')), 'utf-8');
 const categories = JSON.parse(fs.readFileSync(path.join(process.cwd(), '/data/categories.json')), 'utf-8');
 
@@ -23,14 +25,17 @@ let validateBook = [
     check('language').notEmpty().withMessage('Debe ingresar el idioma del libro'),
     check('isbn').notEmpty().withMessage('Debe ingresar el ISBN (identificador internacional de publicaciones)'),
     check('price').notEmpty().withMessage('Debe ingresar el precio del libro').bail()
+    .isNumeric().withMessage('La cantidad ingresada debe ser un número'),
+    check('stock').notEmpty().withMessage('Debe ingresar el stock del libro').bail()
     .isNumeric().withMessage('La cantidad ingresada debe ser un número')
 ];
 
 //===========================================================================
 //===========================================================================
 
-const bookValidator = (req, res, next) => {
-    let genres = req.body.genre;
+const bookValidator = async (req, res, next) => {
+    let genresChosen = req.body.genre;
+    let authorsChosen = req.body.author;
     let errors = validationResult(req);
 
 
@@ -42,32 +47,58 @@ const bookValidator = (req, res, next) => {
             return res.render('products/productList', { books: books, user: user });
         }*/
 
-        let categoryIndexes = []
+        let genreIds = [];
+        let authorIds = [];
 
         /*guarda los indices de las categorias para enviarlos y usarlos para marcar 
         como seleccionadas las opciones en la vista por medio de etiqutas ejs */
-        console.log(genres);
-        if(genres){
-            console.log('En if');
-            if(Array.isArray(genres)){
-                genres.forEach(bookCategory => {
-                    let returnedIndex = categories.findIndex((category) => Number(bookCategory) === category.id);
-                    if(returnedIndex !== -1){
-                        categoryIndexes.push(returnedIndex);
-                    }
-                });
+        console.log(genresChosen);
+        
+        if(genresChosen){
+            //console.log('En if');
+            if(Array.isArray(genresChosen)){
+                for(id of genresChosen){
+                    genreIds.push(Number(id));
+                }
             }else{
-                let returnedIndex = categories.findIndex((category) => Number(genres) === category.id);
-                categoryIndexes.push(returnedIndex);
+                genreIds.push(Number(genresChosen));
+            }
+        }
+        console.log(genreIds);
+
+        if(authorsChosen){
+            //console.log('En if');
+            if(Array.isArray(authorsChosen)){
+                for(id of authorsChosen){
+                    authorIds.push(Number(id));
+                }
+            }else{
+                authorIds.push(Number(authorsChosen));
             }
         }
 
-        let user = finders.searchUserByEmail(req.session.currentUserMail, users);
+        const categories = await db.Genre.findAll({
+            order: [['name', 'ASC']]
+        });
+
+        const editorials = await db.Editorial.findAll({
+            order: [['name', 'ASC']]
+        });
+
+        const authors = await db.Author.findAll({
+            order: [['name', 'ASC']]
+        });
+
+        let user = await finders.searchUserByEmail(req.session.currentUserMail);
         return res.render('products/productCreate', {
             categories,
+            editorials,
+            authors,
+            genreIds,
+            authorIds,
             errors: errors.mapped(),
             old: req.body,
-            categoryIndexes,
+            //categoryIndexes,
             user
         })
     }
