9457126fcf00d1621c470361c4ad8781350d18c2
daylucero <puemaam@gmail.com>
2024-03-10 14:25:32 -0300

    api para un usuario específico

--- controllers/api/usersController.js
+++ controllers/api/usersController.js
@@ -30,6 +30,40 @@ const controller = {
             console.log(error);
         })
     },
+
+    detail: (req, res) => {
+        const userId = req.params.id;
+
+        db.User.findByPk(userId)
+            .then(userDB => {
+                if (!userDB) {
+                    return res.status(404).json({
+                        status: 404,
+                        error: 'Usuario no encontrado',
+                    });
+                }
+
+                const userData = {
+                    id: userDB.id,
+                    first_name: userDB.first_name,
+                    last_name: userDB.last_name,
+                    email: userDB.email,
+                    pf_image_url: `/images/profileImages/${userDB.pf_image}`,
+                };
+
+                return res.json({
+                    //status: 200,
+                    user: userData,
+                });
+
+            }).catch(error => {
+                console.log(error);
+                // return res.status(500).json({
+                //     status: 500,
+                //     error: 'Error interno del servidor',
+                // });
+            });
+    },
 }
 
 module.exports = controller;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,5 +2,6 @@ const router = require ('express').Router();
 const usersController = require ('../../controllers/api/usersController')
 
 router.get('/', usersController.list);
+router.get('/:id', usersController.detail);
 
 module.exports = router;
