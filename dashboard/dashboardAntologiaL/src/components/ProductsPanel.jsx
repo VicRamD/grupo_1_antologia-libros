bf1b022ab41d6227f1ee6cf6de74c346af854302
Carlos <charly_zava@yahoo.com.ar>
2024-03-25 17:51:41 -0300

    Agregué más información al hacer click en el libro

--- dashboard/dashboardAntologiaL/src/components/ProductsPanel.jsx
+++ dashboard/dashboardAntologiaL/src/components/ProductsPanel.jsx
@@ -1,24 +1,38 @@
 import { useState, useEffect } from 'react'
 function ProductsPanel() {
     let [products, setProducts] = useState([]);
+    let [selectedProduct, setSelectedProduct] = useState(null);
     useEffect(()=>{
     fetch('http://localhost:3030/api/books')
         .then(res => res.json())   
         .then(({ products }) => {
             setProducts( products );
-            console.log( products );
           })   
         .catch(err => console.log(err));
     },[]);
+
+    const handleClick = (product) => {
+        setSelectedProduct(product);
+    }
     return (
         <div>
-            <h3>Totales de Productos</h3>
-            <ul>
+        <h3>Totales de Productos</h3>
+        <ul>
             {products.map(product => (
-                    <li key={product.id + product.name}>{product.name}</li>
-                ))}
-            </ul>
-        </div>
+                <li key={product.id + product.name} onClick={() => handleClick(product)}>
+                    {product.name}
+                </li>
+            ))}
+        </ul>
+        {selectedProduct && (
+            <div>
+                <h4>{selectedProduct.name}</h4>
+                <p>Author: {selectedProduct.author}</p>
+                <p>Descripción: {selectedProduct.description}</p>
+                <p>Géneros: {selectedProduct.genres.join(', ')}</p>        
+            </div>
+            )}
+            </div>
     );
 }
 
